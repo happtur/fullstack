bd2c06e84a6e823cd5bb3791a50b0d5be4b3fca0
happtur <andrea.eriksson@helsinki.fi>
2018-01-30 15:00:28 +0200

    teht.3

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Osa = (props) => {
-    return (
-        <div>
-            <p>{props.nimi} {props.tehtavia}</p>
-        </div>
-    )
-}
-
 const Otsikko = (props) => (
     <div>
         <h1>{props.kurssi}</h1>
     </div>
 )
 
+const Osa = (props) =>  (
+    <div>
+        <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+    </div>
+)
+
 const Sisalto = (props) => {
     return (
       <div>
-        <Osa nimi={props.osa1} tehtavia={props.tehtavia1} />
-        <Osa nimi={props.osa2} tehtavia={props.tehtavia2} />
-        <Osa nimi={props.osa3} tehtavia={props.tehtavia3} />
+        <Osa osa={props.osa1} />
+        <Osa osa={props.osa2} />
+        <Osa osa={props.osa3} />
       </div>
     )
   }
@@ -32,23 +30,28 @@ const Yhteensa = (props) => (
 )
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
-
-  return (
-    <div>
-      <Otsikko kurssi={kurssi} />
-      <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2}
-       tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3} />
-      <Yhteensa tehtavia={tehtavia1 + tehtavia2 + tehtavia3} />
-    </div>
-  )
-}
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    }
+    const osa2 = {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    }
+    const osa3 = {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
+  
+    return (
+      <div>
+        <Otsikko kurssi={kurssi} />
+        <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} />
+        <Yhteensa tehtavia={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} />
+      </div>
+    )
+  }
 
 ReactDOM.render(
   <App />,

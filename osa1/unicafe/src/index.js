2e45188c4d477abf353ff5588449c4ef813dc0e7
happtur <andrea.eriksson@helsinki.fi>
2018-01-30 19:34:05 +0200

    teht.7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -18,6 +18,14 @@ class App extends React.Component {
     }
 
     render() {
+        const hyva = this.state.hyva
+        const neutraali = this.state.neutraali
+        const huono = this.state.huono
+    
+        const sum = hyva - huono
+        let total = hyva + neutraali + huono
+        if(total == 0) {total =1}
+
         return (
             <div>
                 <h3>anna palautetta</h3>
@@ -29,6 +37,8 @@ class App extends React.Component {
                 <p>hyvä {this.state.hyva}</p>
                 <p>neutraali {this.state.neutraali}</p>
                 <p>huono {this.state.huono}</p>
+                <p>keskiarvo {(sum/total).toFixed(1)}</p>
+                <p>positiivisia {((hyva/total)*100).toFixed(1)} %</p>
             </div>
         )
     }

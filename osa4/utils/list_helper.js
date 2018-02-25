9aa960fa78953169b10a0381fc1da93a7c926fb9
happtur <andrea.eriksson@helsinki.fi>
2018-02-25 13:36:51 +0200

    teht. 4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,7 +6,37 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, current) => sum + current.likes, 0)
 }
 
+/* const favoriteBlog = (blogs) => {
+    let blogWithMostLikes = 0
+    let mostLikes = 0
+
+    blogs.forEach((blog) => {
+        if(blog.likes > mostLikes) {
+            blogWithMostLikes = blog
+            mostLikes = blog.likes
+        }
+    })
+
+    return blogWithMostLikes
+} */
+
+const favoriteBlog = (blogs) => {
+    let blogWithMostLikes = 0
+
+    const reducer = (mostLikes, current) => {
+        if(current.likes > mostLikes) {
+            blogWithMostLikes = current
+            return current.likes
+        }
+        return mostLikes
+    }
+
+    blogs.reduce(reducer, 0)
+    return blogWithMostLikes
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

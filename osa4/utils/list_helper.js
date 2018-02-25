2b4df281688d39b8eef2836a5e3fbe8e161bdc4b
happtur <andrea.eriksson@helsinki.fi>
2018-02-25 12:54:19 +0200

    teht. 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, current) => sum + current.likes, 0)
+}
+
+module.exports = {
+    dummy,
+    totalLikes
+}
